d7ed502c54d65de7d4e9c0ad6f51444eaadda8fb
karuna <karunakakade8@gmail.com>
2021-04-20 22:57:24 +0530

    Added: Assignments Programs

--- Array/PrimeFactorisation.sh
+++ Array/PrimeFactorisation.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 count=0
 read -p "Enter any number: " num
-
 for ((i=1; i<=$num; i++))
 do
- if [ $((num % i)) -eq 0 ]
+ if [ $((num%i)) -eq 0 ]
  then 
   echo $i
-  num=$((num / i))
-   arr[((count++))]=$i
+  num=$((num/i))
+   arr[$((count++))]=$i
   fi
 done
  echo Array: ${arr[@]}
